8a3f2d8f084b1a73ec63c3b33b82b0560834d56f
r28d <o.mazhula@3dvisual-studio.com>
2025-05-18 16:35:30 +0300

    db upd

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base, Session
 from starlette.middleware.cors import CORSMiddleware
 
 # DATABASE_URL = "sqlite:///./test.db"
-DATABASE_URL = DATABASE_URL = "postgresql://ebook_db_y7e4_user:0xafHPKtkvidYg7wnRrOcJBOGafJ5SP1@dpg-d0kts17fte5s7390ss50-a:5432/ebook_db_y7e4"
+DATABASE_URL = "postgresql://ebook_db_y7e4_user:0xafHPKtkvidYg7wnRrOcJBOGafJ5SP1@dpg-d0kts17fte5s7390ss50-a:5432/ebook_db_y7e4"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
